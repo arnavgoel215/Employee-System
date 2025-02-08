6c0798e10c28436ec4ad36715441b35f2ce5137d
Arnav Goel <arnavgoel13@gmail.com>
2025-02-08 17:58:25 -0600

    Implemented employee list component

---
+++ src/components/Employees.js
@@ -0,0 +1,15 @@
+const Employees = () => {
+    return ( 
+        <table id="employees">
+                <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>Position</th>
+                        <th>Phone Number</th>
+                    </tr>
+                </thead>
+            </table>
+     );
+}
+ 
+export default Employees;
--- src/pages/EmployeeList.js
+++ src/pages/EmployeeList.js
@@ -1,7 +1,10 @@
+import Employees from "../components/Employees";
+
 const EmployeeList = () => {
     return ( 
         <>
             <h2>Employee List</h2>
+            <Employees />
         </>
      );
 }
