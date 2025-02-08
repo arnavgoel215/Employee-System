53d1f36cb42782687d8fc7e9b1a5c8082bcf8935
Arnav Goel <arnavgoel13@gmail.com>
2025-02-08 16:28:57 -0600

    Added current date to home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,7 +1,17 @@
 const Home = () => {
+    const date = new Date();
+    let day = date.getDate();
+    let month = date.getMonth() + 1;
+    let year = date.getFullYear();
+    
+    let currentDate = `${month}/${day}/${year}`
+
     return ( 
         <>
             <h2>Welcome</h2>
+            <article>
+                <p>Today is {currentDate}</p>
+            </article>
         </>
      );
 }
