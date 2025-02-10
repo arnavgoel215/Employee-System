f38a2d08feb0dcc6fc2384aa7ead9103a90a3d59
Arnav Goel <arnavgoel13@gmail.com>
2025-02-09 18:56:17 -0600

    Added sample data to employee list

--- src/components/Employees.js
+++ src/components/Employees.js
@@ -7,6 +7,16 @@ const Employees = () => {
                         <th>Position</th>
                         <th>Phone Number</th>
                     </tr>
+                    <tr>
+                        <td>John Smith</td>
+                        <td>Cashier</td>
+                        <td>555-555-5555</td>
+                    </tr>
+                    <tr>
+                        <td>Jane Doe</td>
+                        <td>Assistant Manager</td>
+                        <td>555-555-5556</td>
+                    </tr>
                 </thead>
             </table>
      );
